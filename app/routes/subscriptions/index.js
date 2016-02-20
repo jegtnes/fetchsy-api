51e081e00897baf325012ba7d128f4436ef75568
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-20 02:11:47 +0000

    implemented get and post subscription

--- app/routes/subscriptions/index.js
+++ app/routes/subscriptions/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 
+var Subscription = require('../../models/subscription.js');
+
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.json({hi: 'what'});
-})
+  Subscription.find(function(err, subscriptions) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(subscriptions);
+  });
+});
+
+router.post('/', function(req, res) {
+  var subscription = new Subscription();
+
+  subscription.shopName = req.body.shopName;
+  subscription.frequency = req.body.frequency;
+  subscription.lastChecked = req.body.lastChecked;
+
+  subscription.save(function(err) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({
+      message: 'Subscription added',
+      data: subscription
+    });
+  });
+});
 
 module.exports = router;
