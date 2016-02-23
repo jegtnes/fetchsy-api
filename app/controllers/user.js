491596c4f5c9fe1c66c38ce9c887c9af1fa3f143
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-23 01:15:16 +0000

    very rudimentary get/post user

---
+++ app/controllers/user.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var User = require('../models/user.js');
+var env     = require('../env');
+
+var router = express.Router();
+var userController = {};
+
+userController.getUsers = function(req, res) {
+  User.find(function(err, users) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.json(users);
+  });
+}
+
+userController.postUser = function(req, res) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user.save(function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    var resourceURI = env.apiURL + '/users/' + user._id;
+
+    return res.status(201).header('Location', resourceURI).end();
+  });
+}
+
+module.exports = userController;
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var subscriptionsController = require('./controllers/subscription.js');
+var usersController = require('./controllers/user.js');
 
 var router = express.Router();
 
@@ -16,5 +17,11 @@ router.route('/subscriptions/:subscriptionId')
   .get(subscriptionsController.getSubscription)
   .put(subscriptionsController.putSubscription)
   .delete(subscriptionsController.deleteSubscription);
+
+router.route('/users')
+  .get(usersController.getUsers)
+  .post(usersController.postUser);
+
+// router.route('/users/:user')
 
 module.exports = router;
