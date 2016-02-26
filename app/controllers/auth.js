9c41e8d3acd5e8624e17eed4319d0618ba561f40
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-26 10:00:10 +0000

    Swapped user auth with API key

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,35 +1,18 @@
 var passport = require('passport');
-var basicStrategy = require('passport-http').BasicStrategy;
+var Strategy = require('passport-http-bearer').Strategy;
 var User = require('../models/user');
 
-passport.use(new basicStrategy(
-  function(email, password, callback) {
-    User.findOne({ email: email }, function(err, user) {
-      if(err) {
-        console.log('error: find user');
-        return callback(err);
-      }
+var config = require('../config');
 
-      if (!user) {
-        console.log('error: no user');
-        return callback(null, false)
-      }
-
-      user.verifyPassword(password, function(err, isMatch) {
-        if (err) {
-          console.log('error: verify password');
-          return callback(err);
-        }
-
-        if (!isMatch) {
-          console.log('error: passwords don\'t match');
-          return callback(null,  false);
-        }
-
-        return callback(null, user);
-      });
-    });
+passport.use(new Strategy(
+  function(token, callback) {
+    if (config.get('apiKey') !== token) {
+      return callback(true);
+    }
+    else {
+      return callback(null, true)
+    }
   }
 ));
 
-exports.isAuthenticated = passport.authenticate('basic', { session: false });
+exports.isAuthenticated = passport.authenticate('bearer', { session: false });
