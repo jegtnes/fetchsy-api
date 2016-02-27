3b274b4c95330dae9890fa9058067e6028d1d2db
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-27 00:50:04 +0000

    Reducing boilerplace

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -4,8 +4,10 @@ var mongoose = require('mongoose');
 var mockgoose = require('mockgoose');
 
 var conf = require('../../app/config');
-var apiSuffix = conf.get('apiSuffix') + "users";
 conf.set('port', conf.get('port') + 1);
+
+var apiSuffix = conf.get('apiSuffix') + "users";
+var authHeader = {'Authorization': 'Bearer ' + conf.get('apiKey')}
 
 mockgoose(mongoose);
 
@@ -13,10 +15,10 @@ var app = require('../../app');
 
 describe('User routes', function() {
   describe('signup', function() {
-    it('should create a user', function(done) {
+    it('should list users', function(done) {
       request(app)
         .get(apiSuffix)
-        .set('Authorization', 'Bearer ' + conf.get('apiKey'))
+        .set(authHeader)
         .expect(200)
         .end(function(err, res) {
           expect(err).to.equal(null);
