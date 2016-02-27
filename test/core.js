f1028fe8282b7660e9588e1921b340dfea280cbe
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-27 22:40:20 +0000

    Implemented fixtures

--- test/core.js
+++ test/core.js
@@ -1,10 +1,23 @@
 var mongoose = require('mongoose');
 var mockgoose = require('mockgoose');
+var fixtures = require('pow-mongoose-fixtures');
 
 var conf = require('../app/config');
 
 before(function(done) {
   mockgoose(mongoose);
+  fixtures.load({
+    User: [
+      {
+        email: 'test1@test.com',
+        password: 'testytest'
+      },
+      {
+        email: 'test2@test.com',
+        password: 'testytest'
+      }
+    ]
+  }, mongoose.connection);
   done();
 });
 
