09481ce7c612ece8a8198bde8b2dfd5943706bae
jegtnes <alex@jegtnes.co.uk>
2016-02-28 17:02:29 +0000

    Test?

---
+++ shipitfile.js
@@ -0,0 +1,30 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+  require('shipit-pm2')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/fetchsy-api',
+      deployTo: '/var/www/fetchsy-api',
+      repositoryUrl: 'git@github.com:jegtnes/fetchsy-api.git',
+      ignores: ['.git', 'node_modules'],
+      rsync: ['--del'],
+      keepReleases: 5,
+      shallowClone: true,
+      pm2: {
+        json: '/var/www/pm2conf.json'
+      },
+    },
+    prod: {
+      servers: 'deploy@46.101.75.113'
+    }
+  });
+
+  shipit.blTask('dependencies', function() {
+    return shipit.remote('cd ' + shipit.releasePath + ' && npm install')
+  });
+
+  shipit.on('updated', function () {
+    return shipit.start('dependencies');
+  });
+};
