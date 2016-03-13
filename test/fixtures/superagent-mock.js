41bd81b19776f409a83c7abe9d90ff49bdb5a2f5
jegtnes <alex@jegtnes.co.uk>
2016-03-13 15:58:30 +0000

    MADE THINGS WORK OMG

--- test/fixtures/superagent-mock.js
+++ test/fixtures/superagent-mock.js
@@ -1,10 +1,33 @@
-var env = require('../../app/env');
+var fs = require('fs');
+var xml2js = require('xml2js').parseString;
 
-console.log(env.apiURL);
+var env = require('../../app/env');
+var conf = require('../../app/config');
 
 module.exports = [{
-  pattern: env.apiURL,
+  pattern: conf.get('etsyBaseURI') + '(.*)/rss',
   fixtures: function(match, params, headers) {
+    if (match[1] === 'MockFullShop') {
+      var file = fs.readFileSync(__dirname + '/etsyFullShop.xml', 'utf8');
+      return file;
+    }
 
-  }
+    if (match[1] === 'MockEmptyShop') {
+      var file = fs.readFileSync(__dirname + '/etsyEmptyShop.xml', 'utf8');
+      return file;
+    }
+  },
+
+  get: function (match, data) {
+    var json = false;
+    xml2js(data, function(err, result) {
+      json = result;
+    });
+
+    return {
+      match: match,
+      body: json,
+      code: 200
+    };
+  },
 }]
